367fe28fc979b8a93eed3c89372e4c3d9fa6c532
Rekuiem84 <morinijoshua@gmail.com>
2024-07-04 17:20:30 +0200

    commit

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -1,26 +1,27 @@
 import { useReadContract } from "wagmi";
-import { config } from "./wagmi";
 
-function App() {
-	const {
-		data: balance,
-		error,
-		isPending,
-	} = useReadContract({
-		config,
-		address: "0xb649FD8F6307B10973795C79155761AD94ED9DB4",
-		args: ["0xb649FD8F6307B10973795C79155761AD94ED9DB4"],
+const contractAddress = "0xb649FD8F6307B10973795C79155761AD94ED9DB4";
+const contractABI = [
+	"function count()",
+	"function increment()",
+	"function decrement()",
+	"function incrementBy(uint value)",
+	"function decrementBy(uint value)",
+	"function reset()",
+];
+
+const App = () => {
+	const { data: count } = useReadContract({
+		address: contractAddress,
+		abi: contractABI,
+		functionName: "count",
 	});
-	console.log(balance);
-	if (isPending) return <div>Loading...</div>;
-	else if (error)
-		return <div>Error: {error.shortMessage || error.message}</div>;
-	else {
-		return (
-			<>
-				<p>Balance: {balance?.toString()}</p>
-			</>
-		);
-	}
-}
+
+	return (
+		<div className="App">
+			<h1>Counter: {count?.toString()}</h1>
+		</div>
+	);
+};
+
 export default App;
